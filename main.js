8c2864f0f977a2c7c78aaedd561e10475926466e
luizeduardo7 <luisedu11201184@gmail.com>
2023-07-11 17:09:07 -0300

    Mean

--- main.js
+++ main.js
@@ -200,9 +200,57 @@ function calculateTime(){
     if(timeManager.time < 40 && scoreManager.try > 0){
         timeManager.countPoint();
         timeManager.updateTimeMeshInScene(scene);
+        meanManager.meanCalculator();
+        meanManager.updateMeanMeshInScene(scene);
     }
     renderer.render(scene, camera);
 }
+
+// mean 
+const meanGroup = new THREE.Group();
+let meanManager = { 
+    mean: '',
+    try: 0,
+    font: undefined,
+    meanMesh: null,
+    fontUri: 'node_modules/three/examples/fonts/gentilis_regular.typeface.json',
+    createMeanMeshInScene: (meanValue, scene) => {
+        new FontLoader().load( meanManager.fontUri, (response) => meanManager.font = response )
+        const textGeom = new TextGeometry( `mean: ${meanValue}`, {
+            font: meanManager.font,
+            size: 2,
+            height: 3,
+            curveSegments: 12,
+        })
+
+        textGeom.computeBoundingBox()
+        const centerOffset = - 0.5 * ( textGeom.boundingBox.max.x - textGeom.boundingBox.min.x )
+
+        meanManager.meanMesh = new THREE.Mesh( textGeom, new THREE.MeshStandardMaterial())
+
+        meanManager.meanMesh.position.x = centerOffset + 17
+        meanManager.meanMesh.position.y = 22
+        meanManager.meanMesh.position.z = -18
+        
+        meanManager.meanMesh.rotation.x = 0
+        meanManager.meanMesh.rotation.y = Math.PI * 2
+        
+        scene.add(meanManager.meanMesh)
+        
+    },
+    updateMeanMeshInScene: (scene) => { 
+        if (meanManager.meanMesh !== null) {
+            scene.remove(meanManager.meanMesh)
+            meanManager.createMeanMeshInScene(meanManager.mean, scene)
+        }
+    },
+    meanCalculator: () => {
+        meanManager.mean = scoreManager.score + '/' + scoreManager.try
+    }
+};
+
+meanManager.createMeanMeshInScene(0, meanGroup)
+scene.add(meanGroup)
 
 // Create Spheres
 function makeSpheres(x, y){
